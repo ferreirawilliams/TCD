b11363cf29812ccbe7235f8a20a4fbdbe3f03c25
Evanilson8674 <50383283+Evanilson8674@users.noreply.github.com>
2019-11-19 16:36:34 -0300

    Update estoque.py

--- estoque.py
+++ estoque.py
@@ -13,8 +13,8 @@ def adiciona():
 		quantidade = input('Digite a Quantidade de ' + nome +'s' + ' que deseja adicionar: ')
 		preço = input('Digite o preço de ' + nome + ':')
 		print('OK!')
-		lista_de_Produtos.append([nome, quantidade])
-		Arquivo_Mercadoria.write('Nome: ' + nome +'|' + 'Quantidade: '+ quantidade + '|' + 'Preço: '+ preço + '\n')
+		lista_de_Produtos.append([nome, quantidade, preço])
+		Arquivo_Mercadoria.write('Nome: ' + nome +' | ' + 'Quantidade: '+ quantidade + ' | ' + 'Preço: '+ preço + '\n')
 	Arquivo_Mercadoria.close()
 	return adiciona
 
@@ -22,27 +22,40 @@ def adiciona():
 def modificar():
 	Arquivo_Mercadoria = open('Arquivo_Mercadoria.txt', 'r', encoding='UTF-8')
 	quant_produto = int(input('Digite a Quantidade de tipos de produtos que deseja modificar: '))
-	
+	#Aqui cada linha do arquivo vira um indice da lista criada no readlines
 	lista_de_Produtos = Arquivo_Mercadoria.readlines()
-	print('OK!')
+	
+	# Esse 'for' pega cada linha da lista e cria indices de cada elemento da linha separado por virgula
+	for i in lista_de_Produtos:
+		lista = i.split()
+		print(lista)
+	"""print('OK!')
 	for i in range(quant_produto):
 		nome = input('Digite o nome do produto que deseja modificar: ').title()
 		modificar_ = input('OK! O que deseja modificar em ' + nome + ':' ).lower()
 		if modificar_ == 'nome': 
 			new_name = input('Digite um novo nome: ')
-
+			lista.pop(1)
+			lista.insert(1,new_name)
+			
 		if modificar_ == 'quantidade':
 			new_quant = input('Digite uma nova quantidade: ')
-
+			lista.pop(4)
+			lista.insert(4,new_quant)
+			print(lista)
+			
 		if modificar_ == 'preço':
 			new_prec = input('Digite o novo preço: ')
-
-	Arquivo_Mercadoria = open('Arquivo_Mercadoria.txt', 'w', encoding='UTF-8')
-	for j in lista_de_Produtos:
-		Arquivo_Mercadoria.write(j + '\n')
-
+			lista.pop(7)
+			lista.insert(7,new_prec)
+			print(lista)
+		
+	
+		print(lista_modificada)"""
 	Arquivo_Mercadoria.close()
-	Arquivo_Mercadoria.close()
+	#Arquivo_Mercadoria = open('Arquivo_Mercadoria.txt', 'w', encoding='UTF-8')
+	#for i in lista_modificada:
+	#	Arquivo_Mercadoria.write(i)
 	return modificar
 
 if pergunta == 'a':
